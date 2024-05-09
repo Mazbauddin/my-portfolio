91e69488811c726a2ac523bcd2341ba567c90709
Mazbauddin <mazbauddin2010@gmail.com>
2024-05-09 23:35:42 +0600

    Luxury Client side start pack complete

---
+++ src/Layouts/MainLayouts.jsx
@@ -0,0 +1,16 @@
+const MainLayouts = () => {
+  return (
+    <div>
+      {/* Navbar */}
+      <div>
+        <h1>Hello nav</h1>
+      </div>
+      {/* Outlet */}
+      <div></div>
+      {/* Footer */}
+      <div></div>
+    </div>
+  );
+};
+
+export default MainLayouts;
