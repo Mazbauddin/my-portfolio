126b6ae87d837f7906977d4968d59dead353b72b
Mazbauddin <mazbauddin2010@gmail.com>
2024-05-14 05:27:16 +0600

    my added food page complete

--- src/Pages/MyAddedFood/MyAddedFood.jsx
+++ src/Pages/MyAddedFood/MyAddedFood.jsx
@@ -1,7 +1,64 @@
+import { useEffect, useState } from "react";
+import useAuthHooks from "../../Hooks/UseAuthHooks";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
 const MyAddedFood = () => {
+  const { user } = useAuthHooks();
+  const [myFoods, setMyFoods] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await axios(
+        `${import.meta.env.VITE_API_URL}/addMyFoods/${user?.email}`
+      );
+      setMyFoods(data);
+    };
+    getData();
+  }, [user]);
+
   return (
-    <div>
-      <h1>mY food</h1>
+    <div className="p-24">
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>Food Image</th>
+              <th>Food Name</th>
+              <th>Food Category</th>
+              <th>Food Origin</th>
+              <th>Price</th>
+              <th>Update</th>
+            </tr>
+          </thead>
+          <tbody>
+            {myFoods?.map((myFoods) => (
+              <tr key={myFoods._id}>
+                <td>
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src={myFoods.image_Url}
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                </td>
+                <td>{myFoods.food_name}</td>
+                <td>{myFoods.food_Category}</td>
+                <td>{myFoods.food_Origin}</td>
+                <td>{myFoods.price}</td>
+                <td>
+                  <Link to={`/update/${myFoods._id}`}>
+                    <button className="btn_wave btn1 rounded-md">Update</button>
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
